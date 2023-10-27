589685f9d13f9a808fd62372ed66055e2f118ac4
MrPotatsus <MrPotatsus@yandex.ru>
2023-10-27 16:32:43 +0300

    individual_task2.py is added

---
+++ individual_task2.py
@@ -0,0 +1,10 @@
+import sys
+
+
+if __name__ == '__main__':
+    sentence = input('Enter sentence: ')
+    if ',' in sentence:
+        n_count = sentence.split(',', 1)[0].count('н')
+        print('Letter "н" count before first comma:', n_count)
+    else:
+        print('There is no commas in sentence', file=sys.stderr)
